2ecc7e00d2707c3b57e7f4d1364498a1c77d2707
Mark-shija <plusmaxtechtz@gmail.com>
2024-05-17 13:12:32 -0700

    Handled user odel on command intrepreter

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import json
 from os.path import exists
+from models.base_model import BaseModel
+from models.user import User
 
 """Stores data in json format"""
 
 class Filestorage:
     """Stores data"""
-    __file_path = file.json
+    __file_path = "file.json"
     __object = {}
 
     def all(self):
@@ -22,7 +24,7 @@ class Filestorage:
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)
         """
-        """"
+        
         json_obj = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as file:
             json.dump(json_object, file)
@@ -30,17 +32,15 @@ class Filestorage:
         for key in self.__objects:
             json_obj[key] = self.__object[key].to_dict()
         with open(self.__file_path, 'w') as file:
-            json.dump(json_obj, file)
-
+            json.dump(json_obj, file)"""
+            
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file (__file_path))
         """
-        """ if exists(self.__file_path):
+        if exists(self.__file_path):
             with open(self.__self_path, 'r') as file:
                 json_obj = json.load(file)
                 for key, value in json_obj.items():
                     class_name = value["__class__"]
                     cls = globals()[class_name]
-                    self.__objects[key] = cls(**value)"""
-        for 
-
+                    self.__objects[key] = cls(**value)
