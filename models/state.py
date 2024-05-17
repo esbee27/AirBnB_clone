6c911ef5866f114ff7e179a52f464019237424ba
Mark-shija <plusmaxtechtz@gmail.com>
2024-05-17 13:27:21 -0700

    Added all model files

---
+++ models/state.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" class state that captures user details """
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """ Class state that inherits from BaseModel """
+    name = ""
